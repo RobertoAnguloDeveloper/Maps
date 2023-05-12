5255b7b93ad22ad9d67c2c8966d1bd5a2b06043f
RobertoAnguloDeveloper <rangulot@outlook.com>
2023-05-11 20:05:14 -0500

    first commit

---
+++ GEO.py
@@ -0,0 +1,8 @@
+# from geopy.geocoders import Nominatim
+
+# geolocation = Nominatim()
+# location = geolocation.reverse("10.3838415,-75.4723243,16")
+
+import plotly.express as px
+import pandas as pd
+
