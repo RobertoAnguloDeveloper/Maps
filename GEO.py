ea5e5dc992f30a3f6a7c55096b22a7688558f02e
RobertoAnguloDeveloper <rangulot@outlook.com>
2023-05-13 23:48:55 -0500

    Learning Flask

--- GEO.py
+++ GEO.py
@@ -6,3 +6,4 @@
 import plotly.express as px
 import pandas as pd
 
+location = "https://www.mapquestapi.com/geocoding/v1/address?key=32efpKKQxCSJchjYCMwAuREIB7ywAOAd&location=10.3930398,-75.5006173"
