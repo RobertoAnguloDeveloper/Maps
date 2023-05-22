3b4cec8ee608e71c86677fc50d9cdd95ed4f080e
RobertoAnguloDeveloper <rangulot@outlook.com>
2023-05-21 23:58:27 -0500

    Vehicle controller

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, jsonify
-import forms
 
 from controller.CurrentPosition import CurrentPosition
 from controller.VehicleController import VehicleController
