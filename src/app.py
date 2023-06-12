5f0a073a2347d74e5406f6ef16ae2329cf7c0b2c
RobertoAnguloDeveloper <rangulot@outlook.com>
2023-06-12 00:37:40 -0300

    Painting routes with folium PolyLine

--- src/app.py
+++ src/app.py
@@ -4,4 +4,7 @@ from CurrentPosition import CurrentPosition
 currentPosition = CurrentPosition().get_current_location3()
 # print(currentPosition)
 m = folium.Map(location=currentPosition, zoom_start=14)
+customIcon = folium.CustomIcon("./src/img/car2.png", icon_size=(50, 50))
+folium.Marker(location=currentPosition, icon=customIcon, draggable=True).add_to(m)
+# folium.Marker(location=currentPosition, draggable=True).add_to(m)
 m.save("index.html")
