9ec60df67ff475f7f30593b3b257a76931b4fa40
RobertoAnguloDeveloper <rangulot@outlook.com>
2023-06-25 18:11:46 -0300

    Only API

--- Testing/Folium.py
+++
@@ -1,5 +0,0 @@
-import folium
-
-m = folium.Map(location=[10.3837753,-75.4727977], zoom_start=15)
-
-m.save("mapfolium.html")
--- Testing/GEO.py
+++
@@ -1,9 +0,0 @@
-# from geopy.geocoders import Nominatim
-
-# geolocation = Nominatim()
-# location = geolocation.reverse("10.3838415,-75.4723243,16")
-
-import plotly.express as px
-import pandas as pd
-
-location = "https://www.mapquestapi.com/geocoding/v1/address?key=32efpKKQxCSJchjYCMwAuREIB7ywAOAd&location=10.3930398,-75.5006173"
